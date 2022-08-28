5bb5e6d79786caf6727afaaae3a2cf60bce17c28
ahmedfahim21 <ahmed.fahim0207@gmail.com>
2022-08-28 20:06:39 +0530

    video 9-Fahim

--- app/src/main/java/com/example/blooddonationapp/MainActivity.java
+++ app/src/main/java/com/example/blooddonationapp/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.blooddonationapp;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private NavigationView nav_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar =findViewById(R.id.toolbar);
+        getSupportActionBar().setTitle("Blood Donation App");
+
+        drawerLayout =findViewById(R.id.drawerLayout);
+        nav_view =findViewById(R.id.nav_view);
+
+
+        ActionBarDrawerToggle toggle =new ActionBarDrawerToggle(MainActivity.this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
 }
